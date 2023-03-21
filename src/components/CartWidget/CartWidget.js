8b14bfb372161565fb699c8ab4cf1ae326b00289
Claudia Garcia <claudiacoderhouse@gmail.com>
2023-03-21 15:38:30 -0300

    Ecommerce de Clau en React primer subida

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,13 @@
+import  cart  from './assets/carrito_compras.png';
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt="carrito de compras"/>
+            0
+        </div>
+    
+    )
+}
+
+export default CartWidget;
