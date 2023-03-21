fd4682e4e778011c30dd48ec3cd6a88796148d2c
Claudia Garcia <claudiacoderhouse@gmail.com>
2023-03-21 17:22:29 -0300

    actulizacion de los componentes para su funcionamiento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer.js/ItemListContainer';
 
 function App() {
   return (
